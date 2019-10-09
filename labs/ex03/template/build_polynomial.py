8376eec34afae13ae3e168f35cc41f21787d67f0
CeliaBenquet <32598028+CeliaBenquet@users.noreply.github.com>
2019-10-09 18:15:19 +0200

    Merge remote-tracking branch 'upstream/master'

--- labs/ex03/template/build_polynomial.py
+++ labs/ex03/template/build_polynomial.py
@@ -3,7 +3,6 @@
 
 import numpy as np
 
-
 def build_poly(x, degree):
     """polynomial basis functions for input data x, for j=0 up to j=degree."""
     # ***************************************************
@@ -11,5 +10,12 @@ def build_poly(x, degree):
     # polynomial basis function: TODO
     # this function should return the matrix formed
     # by applying the polynomial basis to the input data
+
+    matrix=np.ones((len(x),1))
+    
+    for j in range(1, degree+1):
+        matrix = np.c_[matrix, x**(j)]
+    
+    return matrix 
     # ***************************************************
-    raise NotImplementedError
+    #raise NotImplementedError
